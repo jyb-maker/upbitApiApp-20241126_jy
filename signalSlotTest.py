ace2f085d871956e40e0231527fd45bd11518d2b
jyb-maker <bjy7328@naver.com>
2024-11-26 14:04:53 +0900

    update01

--- signalSlotTest.py
+++ signalSlotTest.py
@@ -7,7 +7,7 @@ class Worker(QThread):   # signal class
     signal1 = pyqtSignal(int, int)  # signal method -> 인수로는 슬롯으로 보내려는 값의 타입을 괄호안에 작성
 
     def run(self):
-        self.signal1.emit(1000, 2000)
+        self.signal1.emit(1000,2000)
 
 class MainWindow(QMainWindow): # slot class
     def __int__(self):
